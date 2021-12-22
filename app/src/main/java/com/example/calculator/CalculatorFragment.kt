9b922d95b90c290fe4553ba0866982b4d88f8f63
seogiwon <seokw2240@gmail.com>
2021-12-22 13:08:51 +0900

    error..

--- app/src/main/java/com/example/calculator/CalculatorFragment.kt
+++ app/src/main/java/com/example/calculator/CalculatorFragment.kt
@@ -4,14 +4,45 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import org.w3c.dom.Text
 
-class CalculatorFragment: Fragment() {
+class CalculatorFragment : Fragment() {
+
+
+    fun buttonClicked(v: View){
+    }
+    fun resultButtonClicked(v: View){
+    }
+    fun historyButtonClicked(v: View){
+    }
+    fun clearButtonClicked(v: View){
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
+
+
     ): View? {
-        return inflater.inflate(R.layout.fragment_calculator,null)
+        return inflater.inflate(R.layout.fragment_calculator, null)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        private val expressionTextView: TextView by lazy {
+            view.findViewById<TextView>(R.id.txt_expression)
+        }
+        private val resultTextView: TextView by lazy {
+            view.findViewById<TextView>(R.id.txt_result)
+        }
+
+        private var isOperator = false
+        private var hasOperator = false
+
+
+
     }
 }
--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,23 +2,27 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    val calculatorFragment = CalculatorFragment()
-    val listFragment = ListFragment()
+    private val calculatorFragment = CalculatorFragment()
+    private val listFragment = ListFragment()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_list_calu)
+
+        val bottomNavigationView =
+            findViewById<BottomNavigationView>(R.id.bottom_navigation_list_calu)
         bottomNavigationView.setOnItemReselectedListener {
-            when(it.itemId){
-                R.id.menu_list -> swichFragment(listFragment)
-                R.id.menu_Calculator ->swichFragment(calculatorFragment)
+            when (it.itemId) {
+                R.id.menu_list -> showFragment(listFragment)
+                R.id.menu_Calculator -> showFragment(calculatorFragment)
             }
 
             true
@@ -26,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun  swichFragment(fragment: Fragment){
+    private fun showFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, fragment)
             .commit()
